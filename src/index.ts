166e68339e3a8b8788bf3e22567acd61bb3bbc47
Forrest Wolf <forrest.weiswolf@gmail.com>
2022-12-15 21:14:40 -0500

    Make countInstances actually count instances

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export const countInstances = (patterns: RegExp[], str: string): Record<string, number> => {
   const result: Record<string, number> = {}
   patterns.forEach(pattern => {
-    result[pattern.toString()] = 0
+    result[pattern.toString()] = str.match(pattern)?.length || 0
   })
 
   return result
--- test/index.test.ts
+++ test/index.test.ts
@@ -6,10 +6,19 @@ describe('countInstances', () => {
   })
 
   describe('when passed an array with several patterns', () => {
-    const patterns = [/a/, /\d+/, /\t/]
     it('when passed an empty string, returns the patterns as keys with values of 0', () => {
+      const patterns = [/a/, /\d+/, /\t/]
+
       expect(countInstances(patterns, '')).toEqual({
         '/a/': 0, '/\\d+/': 0, '/\\t/': 0
+      })
+    })
+
+    it('when passed a string that matches the patterns, returns the number of matches as values', () => {
+      const patterns = [/a/g, /\d+/g, /\t/g]
+
+      expect(countInstances(patterns, 'aa12 aaa')).toEqual({
+        '/a/g': 5, '/\\d+/g': 1, '/\\t/g': 0
       })
     })
   })
