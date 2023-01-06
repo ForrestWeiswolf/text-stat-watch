7a41458f895004e640ed7b5a49e802abe4bb0ee3
Forrest Wolf <forrest.weiswolf@gmail.com>
2023-01-05 20:53:27 -0500

    Count instances of a pattern in a file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { readFileSync } from 'fs'
+import { countInstances } from '../src/countInstances'
 
 export default function textStatWatch() {
-  readFileSync(process.argv[2])
+  const text = readFileSync(process.argv[2])?.toString()
+  const regex = new RegExp(process.argv[3])
+  const instanceCounts = countInstances([regex], text || '')
+  const count = instanceCounts[regex.toString()]
+
+  process.stdout.write(String(count))
 }
 
 textStatWatch()
--- test/index.test.ts
+++ test/index.test.ts
@@ -2,14 +2,32 @@ import fs from 'fs'
 import textStatWatch from '../src/index'
 
 jest.mock('fs')
+jest.spyOn(process.stdout, 'write')
 
 describe('text-stat-watch', () => {
-  beforeEach(() =>  {
-    process.argv = 'node index.js foo.txt /\W+/'.split(' ')
-    textStatWatch()
+  beforeEach(() => {
+    const readFileSyncMock = fs.readFileSync as jest.Mock
+    readFileSyncMock.mockReturnValue('foo bar baz')
   })
 
   it('reads the file passed as the first argument', () => {
+    process.argv = 'node index.js foo.txt \w+'.split(' ')
+    textStatWatch()
+
     expect(fs.readFileSync).toBeCalledWith('foo.txt')
+  })
+
+  it('outputs the number of matches of the regex passed as the second argument', () => {
+    process.argv = 'node index.js foo.txt f'.split(' ')
+    textStatWatch()
+
+    expect(process.stdout.write).toBeCalledWith("1")
+  })
+
+  it('outputs the number of matches of a more complicated regex passed as the second argument', () => {
+    process.argv = 'node index.js foo.txt b\\w+'.split(' ')
+    textStatWatch()
+
+    expect(process.stdout.write).toBeCalledWith("2")
   })
 })
