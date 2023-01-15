e782054fbe1133f800d74eb3e559f348283c2414
Forrest Wolf <forrest.weiswolf@gmail.com>
2023-01-15 09:03:26 -0500

    Refactor textStatWatch

--- src/textStatWatch.ts
+++ src/textStatWatch.ts
@@ -1,24 +1,29 @@
 import { countInstances } from './countInstances'
 import watchFile from './watchFile'
 
+const printTextStats = (regexes: RegExp[], text: string) => {
+  const instanceCounts = countInstances(regexes, text)
+
+  Object.entries(instanceCounts).forEach(([regex, count]) => {
+    console.log(`${regex}: ${count}`)
+  })
+}
+
 export default function textStatWatch() {
   if ([...process.argv].length < 4) {
     throw new Error('Must pass exactly two arguments')
   }
 
   let isFirstRun = true
+  const regexes = [...process.argv].slice(3).map(expression => new RegExp(expression))
 
   watchFile(process.argv[2], (text) => {
-    const regexes = [...process.argv].slice(3).map(expression => new RegExp(expression))
-    const instanceCounts = countInstances(regexes, text)
-
     if (!isFirstRun) {
       console.log('')
     } else {
       isFirstRun = false
     }
-    Object.entries(instanceCounts).forEach(([regex, count]) => {
-      console.log(`${regex}: ${count}`)
-    })
+
+    printTextStats(regexes, text)
   })
 }
